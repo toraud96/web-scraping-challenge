5da57bcabca6c35f3af4f1f04daeabea27dc61bf
toraud96 <56538394+toraud96@users.noreply.github.com>
2020-03-07 21:18:53 -0600

    update

--- app.py
+++ app.py
@@ -1,48 +1,30 @@
 from flask import Flask, render_template, redirect
-# from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo
 import scrape_mars
 
-# #################################################
-# # Setup Dictionary of Mars_data
-# #################################################
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/"
+mongo = PyMongo(app)
+
+# Or set inline
+# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
 
 
-#################################################
-# Flask Routes
-#################################################
-# Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
-    # Find one record of data from the mongo database
-    mars_data = mongo.db.collection.find_one()
-
-    # Return template and data
-    return render_template("index.html", mars=mars_data)
-
-# @app.route('/')
-# def index():
-#
-#     # Store the entire mars news collection in a list
-#     mars = list(db.nasa.find(news_title, news_p))
-#     print(mars)
-#
-#     # Return the template with the news passed in
-#     return render_template('index.html', mars=mars)
+def index():
+    listings = mongo.db.listings.find_one()
+    return render_template("index.html", listings=listings)
 
 
-# Route that will trigger the scrape function
 @app.route("/scrape")
-def scrape():
+def scraper():
+    listings = mongo.db.listings
+    listings_data = scrape_mars.scrape()
+    listings.update({}, listings_data, upsert=True)
+    return redirect("/", code=302)
 
-    # Run the scrape function
-    mars_data = scrape_mars.scrape_info()
-
-    # Update the Mongo database using update and upsert=True
-    mongo.db.collection.update({}, mars_data, upsert=True)
-
-    # Redirect back to home page
-    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
-
