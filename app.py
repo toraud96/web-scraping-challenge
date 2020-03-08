5216a85027fa24403bf9a8ea31532e818fec6f10
toraud96 <56538394+toraud96@users.noreply.github.com>
2020-03-07 19:37:53 -0600

    update

--- app.py
+++ app.py
@@ -1,30 +1,48 @@
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
-import scrape_craigslist
+# from flask_pymongo import PyMongo
+import scrape_mars
 
-app = Flask(__name__)
-
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/craigslist_app"
-mongo = PyMongo(app)
-
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/craigslist_app")
+# #################################################
+# # Setup Dictionary of Mars_data
+# #################################################
 
 
+#################################################
+# Flask Routes
+#################################################
+# Route to render index.html template using data from Mongo
 @app.route("/")
-def index():
-    listings = mongo.db.listings.find_one()
-    return render_template("index.html", listings=listings)
+def home():
+    # Find one record of data from the mongo database
+    mars_data = mongo.db.collection.find_one()
+
+    # Return template and data
+    return render_template("index.html", mars=mars_data)
+
+# @app.route('/')
+# def index():
+#
+#     # Store the entire mars news collection in a list
+#     mars = list(db.nasa.find(news_title, news_p))
+#     print(mars)
+#
+#     # Return the template with the news passed in
+#     return render_template('index.html', mars=mars)
 
 
+# Route that will trigger the scrape function
 @app.route("/scrape")
-def scraper():
-    listings = mongo.db.listings
-    listings_data = scrape_craigslist.scrape()
-    listings.update({}, listings_data, upsert=True)
-    return redirect("/", code=302)
+def scrape():
 
+    # Run the scrape function
+    mars_data = scrape_mars.scrape_info()
+
+    # Update the Mongo database using update and upsert=True
+    mongo.db.collection.update({}, mars_data, upsert=True)
+
+    # Redirect back to home page
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 from splinter import Browser
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -6,10 +12,243 @@ import pymongo
 import requests
 
 
-def init_browser():
-    # @NOTE: Replace the path with your actual path to the chromedriver
-     executable_path = {'executable_path': 'chromedriver.exe'}
-     browser = Browser('chrome', **executable_path, headless=False)
-     return browser
+# In[2]:
 
+
+executable_path = {'executable_path': 'chromedriver'}
+browser = Browser('chrome', **executable_path, headless=False)
+
+
+# ### NASA Mars News
+
+# In[3]:
+
+
+url = 'https://mars.nasa.gov/news/'
+browser.visit(url)
+
+
+# In[4]:
+
+
+html = browser.html
+soup = BeautifulSoup(html, 'html.parser')
+
+
+# In[5]:
+
+
+print(soup.prettify())
+
+
+# In[6]:
+
+
+article = soup.find("div", class_="list_text")
+news_title = article.find("div", class_="content_title").text
+news_p = article.find("div", class_="article_teaser_body").text
+print(news_title)
+print(news_p)
+
+
+# ### JPL Mars Space Images - Featured Image
+
+# In[7]:
+
+
+image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+browser.visit(image_url)
+
+
+# In[8]:
+
+
+html = browser.html
+soup = BeautifulSoup(html, 'html.parser')
+
+
+# In[9]:
+
+
+main_url ='https://www.jpl.nasa.gov'
+
+
+# In[10]:
+
+
+featured_image_url = soup.find('article')['style'].replace('background-image: url(','').replace(');', '')[1:-1]
+
+
+# In[11]:
+
+
+featured_image_url=main_url+featured_image_url
+print(featured_image_url )
+
+
+# ### Mars Weather
+
+# In[12]:
+
+
+# tweet_url='https://twitter.com/marswxreport?lang=en'
+# browser.visit(tweet_url)
+
+
+# In[13]:
+
+
+# html = browser.html
+# soup = BeautifulSoup(html, 'html.parser')
+
+
+# In[14]:
+
+
+# print(soup.prettify())
+
+
+# In[15]:
+
+
+main_tweet="https://twitter.com"
+
+
+# In[16]:
+
+
+# mars_weather=soup.find('div',class_='nan')
+
+
+# In[17]:
+
+
+# mars_weather=main_tweet+mars_weather
+# print(mars_weather)
+
+
+# In[18]:
+
+
+#url to twitter page for mars weather
+tweet_url = "https://twitter.com/marswxreport?lang=en"
+tweet_html_content = requests.get(tweet_url).text
+soup = BeautifulSoup(tweet_html_content, "lxml")
+tweet_list = soup.find_all('div', class_="js-tweet-text-container")
+#empty list to hold tweet we are going to keep, used to strip useless content from string
+holds_tweet = []
+# Loop that scans every tweet and searches specifically for those that have weather info
+for tweets in tweet_list: 
+    tweet_body = tweets.find('p').text
+    if 'InSight' and 'sol' in tweet_body:
+        holds_tweet.append(tweet_body)
+        #break statement to only print the first weather tweet found
+        break
+    else: 
+        #if not weather related skip it and try again
+        pass
+    
+#cleaned up tweet removes unncessary link to twitter image included in string, :-26 removes the last 26 characters which is the length of the img url
+#after reviewing several links they all appear to work with the value of -26
+mars_weather = ([holds_tweet[0]][0][:-26])
+tweet_img_link = ([holds_tweet[0]][0][-26:])
+print(f"{mars_weather}: {tweet_img_link}")
+
+
+# ### Mars Facts
+
+# In[19]:
+
+
+facts_url='https://space-facts.com/mars/'
+mars_facts=pd.read_html(facts_url)
+
+
+# In[20]:
+
+
+facts_df=mars_facts[0]
+facts_df.head()
+
+
+# In[21]:
+
+
+facts_df.columns = ['Mars', 'Info']
+html_table = facts_df.to_html()
+html_table
+
+
+# ### Mars Hemispheres
+
+# In[22]:
+
+
+hem_url='https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+browser.visit(hem_url)
+
+
+# In[23]:
+
+
+urls = [
+    'https://astrogeology.usgs.gov/search/map/Mars/Viking/cerberus_enhanced',
+    'https://astrogeology.usgs.gov/search/map/Mars/Viking/schiaparelli_enhanced',
+    'https://astrogeology.usgs.gov/search/map/Mars/Viking/syrtis_major_enhanced',
+    'https://astrogeology.usgs.gov/search/map/Mars/Viking/valles_marineris_enhanced'
+]
+images=[]
+
+
+# In[ ]:
+
+
+
+
+
+# In[24]:
+
+
+# info = soup.find("div", class_="item")
+
+
+# In[25]:
+
+
+# titles=[]
+# hemisphere_img_urls=[]
+
+
+# In[26]:
+
+
+# hem_main='https://astrogeology.usgs.gov'
+
+
+# In[27]:
+
+
+for x in urls:
+        print(x)
+        album={}
+        browser.visit(x)
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+        mars_title=soup.find("h2", {"class":"title"}).get_text()
+        album["title"]=mars_title
+        images.append(album)
+        browser.back()
+        
+
+
+# In[28]:
+
+
+images
+
+
+# In[29]:
+
+
+browser.quit()
 
